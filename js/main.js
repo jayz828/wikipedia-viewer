7fa44cf58145b5763c5f17874e8e0d3d79811372
Jason Natividad <jainatividad@yahoo.com>
2017-02-27 23:20:04 -0800

    Start working on sending an api request to wikipedia to get articles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-(function(){
+
+
+(function Main(){
+
+	"use strict";
+
+
 
 
 
@@ -16,6 +22,9 @@
 
 
 
+// "https://en.wikipedia.org/w/api.php?action=query&titles=" + searchTerm + "&prop=revisions&rvprop=content&format=jsonfm"
+
+	
 
 
 
@@ -23,10 +32,70 @@ function search() {
 
 	var searchTerm = document.getElementById("search-term").value;
 
-	alert(searchTerm);
+	var xhr = new XMLHttpRequest();
+	var json;
+
+
+
+
+    xhr.onreadystatechange = function() {
+
+        if (xhr.readyState === 4 && xhr.status === 200){
+
+        	
+            json = JSON.parse(xhr.responseText);
+            console.log(json);
+ 
+
+
+        }
+
+    };
+
+    xhr.open("GET","https://en.wikipedia.org/w/api.php?action=query&titles=" + searchTerm + "&prop=revisions&rvprop=content&format=json"
+, true);
+    xhr.setRequestHeader( 'Api-User-Agent', 'Example/1.0' );
+    xhr.send();
+
+
 
 }
 
 
+// function search() {
 
-})();
+// 	var searchTerm = document.getElementById("search-term").value;
+
+// 	// Using XMLHttpRequest
+// 	var xhr = new XMLHttpRequest();
+// xhr.setRequestHeader( 'Api-User-Agent', 'Example/1.0' );
+
+// // Using jQuery
+// $.ajax( {
+//     url: "https://en.wikipedia.org/w/api.php?action=query&titles=" + searchTerm + "&prop=revisions&rvprop=content&format=json",
+//     data: queryData,
+//     dataType: 'json',
+//     type: 'POST',
+//     headers: { 'Api-User-Agent': 'Example/1.0' },
+//     success: function(data) {
+//        // do something with data
+//        console.log(data);
+//     }
+// } );
+
+// // Using mw.Api, specify it when creating the mw.Api object
+// var api = new mw.Api( {
+//     ajax: {
+//         headers: { 'Api-User-Agent': 'Example/1.0' }
+//     }
+// } );
+// // api.get( {...} ).done(function(data) {
+// //     // do something with data
+// // });
+
+// }
+
+
+
+
+})();   // End IIFE Main
