3e605800f26c101d7622a4936c595310c90bdf7d
Jason Natividad <jainatividad@yahoo.com>
2017-02-27 22:18:56 -0800

    Finish random button functionality

--- js/main.js
+++ js/main.js
@@ -3,8 +3,16 @@
 
 
 	var searchBtn = document.getElementById("search-btn");
+	var randomBtn = document.getElementById("random-button");
 
 	searchBtn.addEventListener("click", search);
+	randomBtn.addEventListener("click", function() {
+		var win = window.open("https://en.wikipedia.org/wiki/Special:Random", "_blank");
+		win.focus();
+	});
+
+
+
 
 
 
@@ -12,7 +20,11 @@
 
 
 function search() {
-	alert("test");
+
+	var searchTerm = document.getElementById("search-term").value;
+
+	alert(searchTerm);
+
 }
 
 
