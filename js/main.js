4deb0a78fb46642fa255446b74aef84cd73660cc
Jason Natividad <jainatividad@yahoo.com>
2017-02-27 21:27:53 -0800

    Add event listener for search button

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function(){
+
+
+
+	var searchBtn = document.getElementById("search-btn");
+
+	searchBtn.addEventListener("click", search);
+
+
+
+
+
+
+function search() {
+	alert("test");
+}
+
+
+
+})();
